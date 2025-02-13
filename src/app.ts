e4c8964aa1d63ab990562ce93dfe7bd7f13b1b14
programmer-nur <pesfootball83@gmail.com>
2025-02-13 06:38:11 +0600

    Add Prisma client and database schema with user and admin models

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express, { Application } from "express";
 import cors from "cors";
 
-const app = express();
+const app: Application = express();
 
 app.use(cors());
 
